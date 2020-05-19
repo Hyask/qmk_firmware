b8b2d1c7be208f33465ff8992cf1a2b845b40fe3
Skia <skia@libskia.so>
2020-05-19 23:01:00 +0200

    First Joystick

---
+++ keyboards/joystick/skia_stick/config.h
@@ -0,0 +1,51 @@
+/*
+Copyright 2020 Skia <skia@libskia.so>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x5314
+#define PRODUCT_ID      0x4000
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    Skia
+#define PRODUCT         Joystick
+#define DESCRIPTION     Fully functional joystick
+
+/* key matrix size */
+#define MATRIX_ROWS 1
+#define MATRIX_COLS 1
+
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE    5
+
+#define TAPPING_TERM 500
+
+
+#define MATRIX_COL_PINS { F4 }
+#define MATRIX_ROW_PINS { F5 }
+#define UNUSED_PINS
+
+#define BACKLIGHT_PIN B6
+
+#define RGB_DI_PIN F6
+
+#define ADC_PIN F6
+
---
+++ keyboards/joystick/skia_stick/skia_stick.c
@@ -0,0 +1,1 @@
+#include "skia_stick.h"
